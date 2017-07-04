671e06550ad31591859f0abd372254a1aafcc69c
owisho <owisho.java@gmail.com>
2017-07-04 18:58:36 +0800

    提交jsonp测试代码

---
+++ src/main/java/per/owisho/learn/jsonpweb/CrossDomainMsg.java
@@ -0,0 +1,32 @@
+package per.owisho.learn.jsonpweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+
+public class CrossDomainMsg extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		Map<String,String> map = new HashMap<String,String>();
+		map.put("a", "1");
+		map.put("b", "2");
+		map.put("c", "3");
+		map.put("d", "4");
+		map.put("e", "5");
+		String callback = req.getParameter("callback");
+		writer.write(JSONObject.fromObject(map).toString()+"."+callback);
+	}
+	
+}
